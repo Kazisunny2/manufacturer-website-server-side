be04e5d00d77b611c492aff7436d2eb01fb511a0
Kazisunny2 <saidul.bit@gmail.com>
2022-05-24 20:47:50 +0200

    env setup

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://tools_admin:<password>@cluster0.js4afvq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.js4afvq.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
 
 app.get("/", (req, res) => {
   res.send("Hello From Tools Manufacturer!");
