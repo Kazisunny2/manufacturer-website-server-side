03221fc0dd46b92de86c99ec6999b6c14517c275
Kazisunny2 <saidul.bit@gmail.com>
2022-05-26 17:30:15 +0200

    jwt token added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -42,7 +43,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIN: "1h" }
+      );
+      res.send({ result, token });
     });
 
     //order purchase
